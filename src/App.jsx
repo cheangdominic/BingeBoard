9c7a70dd201dcdfa80ba286c0e092df709f0fbed
Jbalfour5 <jbalfour5@my.bcit.ca>
2025-05-06 10:32:40 -0700

    Configures app.jsx to work with react routing allowing us to have mutliple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import './App.css';
+
 import Landing from './landing/Landing.jsx'; 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Landing/>
-
-    </>
+    <Router> 
+      <Routes> 
+        <Route path="/" element={<Landing />} />  
+      </Routes>
+    </Router>
   )
 }
 
