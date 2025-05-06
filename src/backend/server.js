cea6195534fb708ec087c4ce39d0047b2accf83b
Jbalfour5 <jbalfour5@my.bcit.ca>
2025-05-06 14:34:47 -0700

    Added a server.js file and required files to allow our apps server to connect with MongoDB

---
+++ src/backend/server.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+require("./utils.js");
+
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const Joi = require('joi');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.urlencoded({extended: false}));
+
+var {database} = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+})
+
+app.use(session({ 
+    secret: node_session_secret,
+	store: mongoStore,
+	saveUninitialized: false, 
+	resave: true
+}
+));
+
+app.listen(port, () => {
+	console.log("Node application listening on port "+port);
+}); 
