e9cb25eb88143f9f5bcef38792cb75ef66e8dc19
cheangdominic <dcheang@my.bcit.ca>
2025-05-06 11:59:07 -0700

    Replaced the placeholder text within the feature cards to accurately represent each feature

--- src/landing/FeatureCards.jsx
+++ src/landing/FeatureCards.jsx
@@ -23,9 +23,10 @@ function FeatureCards() {
               Connect and Share
             </div>
             <p class="text-[#ECE6DD] text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
+            Binging doesn't have to be lonely. 
+            BingeBoard lets you connect with friends, 
+            share what you’re watching, and discover 
+            new favorites together—making your streaming experience social and fun.
             </p>
           </div>
         </div>
@@ -43,9 +44,9 @@ function FeatureCards() {
               Save For Later
             </div>
             <p class="text-[#ECE6DD] text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
+            Never forget that show you wanted to watch but never found the time for. 
+            With BingeBoard, you can build and manage your personal watchlist—so 
+            every must-watch moment is right where you left it.
             </p>
           </div>
         </div>
@@ -63,9 +64,10 @@ function FeatureCards() {
               Browse Your Next Watch
             </div>
             <p class="text-[#ECE6DD] text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
+            Not sure what to watch next? 
+            BingeBoard helps you discover new shows based on your interests, 
+            trending picks, and what your friends are watching—so your next 
+            binge is always one click away.
             </p>
           </div>
         </div>
