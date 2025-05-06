e5ea504f3575a062ec01702557fdd2c6b7c664ef
cheangdominic <dcheang@my.bcit.ca>
2025-05-05 17:38:59 -0700

    Created a banner which displays the brand's motto and a button for the user to get started

--- src/landing/Landing.jsx
+++ src/landing/Landing.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import TopNavbar from './TopNavbar.jsx'
+import MottoBanner from './MottoBanner.jsx'
 
 function Landing() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function Landing() {
   return (
     <>
     <TopNavbar/>
+    <MottoBanner/>
     </>
   )
 }
