d13a1333e584a48c6484e2012648ee38a885bef6
Jbalfour5 <jbalfour5@my.bcit.ca>
2025-05-07 12:56:06 -0700

    Changed the bottom navbar to be responsive working on all screen sizes

--- src/frontend/home/BottomNavbar.jsx
+++ src/frontend/home/BottomNavbar.jsx
@@ -4,53 +4,53 @@ import { BiHome, BiSearch, BiChat, BiUserCircle, BiPlus } from "react-icons/bi";
 export default function BottomNavbar() {
   return (
     <header className="fixed bottom-0 w-full z-50 bg-[#000000]">
-      <nav
-        aria-label="Global"
-        className="relative flex justify-between items-center p-8"
-      >
-        <div className="absolute left-1/2 -translate-x-1/2">
-          <PopoverGroup className="flex items-center justify-center w-full">
-            <div className="flex divide-x divide-white/15">
-              <a
-                href="#"
-                className="flex flex-col items-center text-[#ECE6DD] text-sm font-semibold py-2.5 px-5"
-              >
-                <BiHome size={24} />
-                Home
-              </a>
-              <a
-                href="#"
-                className="flex flex-col items-center text-[#ECE6DD] text-sm font-semibold py-2.5 px-5"
-              >
-                <BiSearch size={24} />
-                Search
-              </a>
-            </div>
-
+      <nav aria-label="Global" className="flex items-center relative h-16">
+        <PopoverGroup className="flex w-full justify-between items-end h-full">
+          {/* Left Buttons */}
+          <div className="flex flex-1 h-full divide-x divide-white/15 pr-8 md:pr-10 lg:pr-12">
             <a
               href="#"
-              className="flex flex-col items-center text-[#ECE6DD] text-sm font-semibold -translate-y-4 border-2 border-white/15 rounded-full p-2 bg-black mx-4"
+              className="flex-1 flex flex-col items-center justify-end text-[#ECE6DD] text-xs md:text-sm font-semibold pb-2"
             >
-              <BiPlus size={40} />
+              <BiHome size={20} className="md:w-6 md:h-6" />
+              <span className="mt-1">Home</span>
             </a>
+            <a
+              href="#"
+              className="flex-1 flex flex-col items-center justify-end text-[#ECE6DD] text-xs md:text-sm font-semibold pb-2"
+            >
+              <BiSearch size={20} className="md:w-6 md:h-6" />
+              <span className="mt-1">Search</span>
+            </a>
+          </div>
 
-            <div className="flex divide-x divide-white/15">
-              <a
-                href="#"
-                className="flex flex-col items-center text-[#ECE6DD] text-sm font-semibold py-2.5 px-5"
-              >
-                <BiChat size={24} />
-                Social
-              </a>
-              <a
-                href="#"
-                className="flex flex-col items-center text-[#ECE6DD] text-sm font-semibold py-2.5 px-5"
-              >
-                <BiUserCircle size={24} />
-                Profile
-              </a>
-            </div>
-          </PopoverGroup>
+          {/* Right Buttons */}
+          <div className="flex flex-1 h-full divide-x divide-white/15 pl-8 md:pl-10 lg:pl-12">
+            <a
+              href="#"
+              className="flex-1 flex flex-col items-center justify-end text-[#ECE6DD] text-xs md:text-sm font-semibold pb-2"
+            >
+              <BiChat size={20} className="md:w-6 md:h-6" />
+              <span className="mt-1">Social</span>
+            </a>
+            <a
+              href="#"
+              className="flex-1 flex flex-col items-center justify-end text-[#ECE6DD] text-xs md:text-sm font-semibold pb-2"
+            >
+              <BiUserCircle size={20} className="md:w-6 md:h-6" />
+              <span className="mt-1">Profile</span>
+            </a>
+          </div>
+        </PopoverGroup>
+
+        {/* Centered Circle Button */}
+        <div className="absolute left-1/2 -translate-x-1/2 bottom-0">
+          <a
+            href="#"
+            className="flex items-center justify-center w-20 h-20 md:w-24 md:h-24 lg:w-24 lg:h-24 border-2 border-white/15 rounded-full bg-black transform transition-all duration-300"
+          >
+            <BiPlus className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 text-[#ECE6DD]" />
+          </a>
         </div>
       </nav>
     </header>
