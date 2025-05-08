675f34a0a3252cde68dc3c8fbfcece2b8424b332
Tyrone Cheang <tcheang@my.bcit.ca>
2025-05-08 15:51:08 -0700

    Added skeleton review cards on the home page with a skeleton filter menu to filter reviews shown on the home page

--- src/frontend/home/Home.jsx
+++ src/frontend/home/Home.jsx
@@ -2,12 +2,14 @@ import BottomNavbar from './BottomNavbar.jsx';
 import TrendingCarousel from './TrendingCarousel.jsx';
 import FriendsRecentlyWatched from './FriendsRecentlyWatched.jsx';
 import RecommendedByFriends from './RecommendedByFriends.jsx';
+import RecentReviewsFiltered from './RecentReviewsFiltered.jsx';
 
 function Home() {
   return (
     <>
     <TrendingCarousel/>
     <FriendsRecentlyWatched/>
+    <RecentReviewsFiltered/>
     <RecommendedByFriends/>
     <BottomNavbar/>
     </>
