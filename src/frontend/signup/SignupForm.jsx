81b3db34999a4ea5f86cc48bf767836266cd0315
cheangdominic <dcheang@my.bcit.ca>
2025-05-08 18:26:22 -0700

    Fixed minor capitalization errors in the signup and login page

--- src/frontend/signup/SignupForm.jsx
+++ src/frontend/signup/SignupForm.jsx
@@ -105,7 +105,7 @@ function SignupForm() {
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img className="mx-auto h-10 w-auto" src="src/assets/BingeBoard Icon.svg" alt="BingeBoard Logo" />
-        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-[#ffffff]">Sign Up For An Account</h2>
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-[#ffffff]">Sign Up for an Account</h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
