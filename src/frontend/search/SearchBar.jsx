ee543ab2b65dd62a68cfc7103fe2df093604170c
Tyrone Cheang <tcheang@my.bcit.ca>
2025-05-15 22:33:26 -0700

    Updated styling for search bar to match app color palette more

--- src/frontend/search/SearchBar.jsx
+++ src/frontend/search/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { Search } from "lucide-react";
+import { FaArrowAltCircleRight } from "react-icons/fa";
 
 export default function SearchBar({ query, setQuery, onSearch }) {
   const handleSubmit = (e) => {
@@ -10,21 +11,21 @@ export default function SearchBar({ query, setQuery, onSearch }) {
   return (
     <div className="mt-4 px-4 sm:px-0">
       <form onSubmit={handleSubmit} className="w-full max-w-lg mx-auto">
-
         <div className="relative">
+          <Search className="absolute left-3 top-2.5 text-gray-400 w-5 h-5" />
           <input
             id="search"
             type="text"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
-            placeholder="Search..."
-            className="block w-full rounded-md bg-white px-4 py-2 text-base text-gray-900 outline outline-2 outline-gray-400 placeholder:text-gray-400 focus:outline-2 focus:outline-[#1963da] transition"
+            placeholder="Search for a TV show..."
+            className="w-full pl-10 pr-4 py-2 rounded-xl bg-zinc-800 text-white focus:outline-none focus:ring-2 focus:ring-yellow-500"
           />
           <button
             type="submit"
-            className="absolute inset-y-0 right-1 flex items-center px-3 text-[#1963da] hover:text-[#ebbd34] focus:outline-none"
+            className="absolute inset-y-0 right-1 flex items-center px-3 text-[#1963da] hover:text-yellow-500 focus:outline-none"
           >
-            <Search size={20} />
+            <FaArrowAltCircleRight size={20} />
           </button>
         </div>
       </form>
