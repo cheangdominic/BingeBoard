837b90988414d3ffec23bde603ce817784eca08a
cheangdominic <dcheang@my.bcit.ca>
2025-05-20 15:33:42 -0700

    Removed redirect in user profiles

--- src/frontend/social/UserProfile.jsx
+++ src/frontend/social/UserProfile.jsx
@@ -1,7 +1,5 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from "../../context/AuthContext";
 import { motion } from "framer-motion";
 import ProfileCard from "../../profile/ProfileCard.jsx";
 import RecentlyWatched from "../../profile/RecentlyWatched.jsx";
@@ -37,15 +35,6 @@ function UserProfile() {
         setUser(null);
       });
   }, [username]);
-
-  const { currentUser } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!currentUser) {
-      navigate('/login');
-    }
-  }, [currentUser, navigate]);
 
   if (!user) return;
 
